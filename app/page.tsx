22d4591d1550b3d3fad0837a8977c17814b84656
newweijun <newweijun@graduate.utm.com>
2025-11-23 14:51:33 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to 0nce Lost and Found System</h1>
+      <h1>Welcome to 0nce Lost and Found</h1>
     </div>
   );
 }
