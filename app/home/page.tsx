73a5cae5a4e7dffbb1a32c5f2cb5d26530406d17
newweijun <newweijun@graduate.utm.com>
2025-11-23 15:11:10 +0800

    route setup

---
+++ app/home/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>test home</div>;
+};
+
+export default page;
---
+++ app/lostfound/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>test lost and found</div>;
+};
+
+export default page;
