7005bab8d5afc6934ef1c55fafff40ac443743ae
newweijun <newweijun@graduate.utm.com>
2025-11-25 15:15:21 +0800

    Item data dynamic

--- app/component/Cards/ItemCard.tsx
+++ app/component/Cards/ItemCard.tsx
@@ -3,7 +3,20 @@ import React from "react";
 import { AiOutlineCalendar } from "react-icons/ai";
 import { VscLocation } from "react-icons/vsc";
 
-const ItemCard = () => {
+type ItemCardProps = {
+  type: string;
+  itemName: string;
+  location: string;
+  date: string;
+  photoURL: string;
+};
+const ItemCard = ({
+  type,
+  itemName,
+  location,
+  date,
+  photoURL,
+}: ItemCardProps) => {
   return (
     <div className="w-[30%] h-fit bg-[#EBECF1] rounded-lg shadow-md">
       {/* UPPER */}
@@ -11,28 +24,28 @@ const ItemCard = () => {
         {/* PHOTO */}
         <div>
           <Image
-            src="/Lost_sample.png"
+            src={photoURL}
             alt="lost item"
             width={200}
             height={200}
             className="rounded-lg w-full h-fit"
           />
           <div className="absolute bottom-2 left-2 bg-white px-4 py-1 rounded-full text-black font-semibold shadow z-40">
-            Lost
+            {type}
           </div>
         </div>
       </div>
       {/* BOTTOM */}
       <div className="flex flex-row w-full h-fit items-center px-3 py-2 justify-between">
-        <span className="text-[#808080] text-xl">Earphone</span>
+        <span className="text-[#808080] text-xl">{itemName}</span>
         <div className="flex flex-row gap-2">
           <div className="flex items-center gap-1">
             <AiOutlineCalendar className="w-4 h-4 text-[#1A8A94]" />
-            <span className="text-[#808080] text-md">11/11/25</span>
+            <span className="text-[#808080] text-md">{date}</span>
           </div>
           <div className="flex items-center gap-1">
             <VscLocation className="w-5 h-5 text-red-500 " />
-            <span className="text-[#808080] text-md">Mpk5</span>
+            <span className="text-[#808080] text-md">{location}</span>
           </div>
         </div>
       </div>
--- app/component/ItemList.tsx
+++ app/component/ItemList.tsx
@@ -1,18 +1,77 @@
 import React from "react";
 import ItemCard from "./Cards/ItemCard";
 
+const ItemDataSample = [
+  {
+    id: 1,
+    type: "Lost",
+    name: "Earphones",
+    date: "11/11/25",
+    location: "Mpk5",
+    photoUrl: "/Lost_sample.png",
+  },
+  {
+    id: 2,
+    type: "Found",
+    name: "Wallet",
+    date: "15/11/25",
+    location: "Library",
+    photoUrl: "/Lost_sample.png",
+  },
+  {
+    id: 3,
+    type: "Lost",
+    name: "Umbrella",
+    date: "10/11/25",
+    location: "Cafeteria",
+    photoUrl: "/Lost_sample.png",
+  },
+  {
+    id: 4,
+    type: "Found",
+    name: "Laptop",
+    date: "12/11/25",
+    location: "Lecture Hall",
+    photoUrl: "/Lost_sample.png",
+  },
+  {
+    id: 5,
+    type: "Lost",
+    name: "Backpack",
+    date: "09/11/25",
+    location: "Parking Lot",
+    photoUrl: "/Lost_sample.png",
+  },
+  {
+    id: 6,
+    type: "Found",
+    name: "Keys",
+    date: "13/11/25",
+    location: "Gym",
+    photoUrl: "/Lost_sample.png",
+  },
+  {
+    id: 7,
+    type: "Lost",
+    name: "Sunglasses",
+    date: "08/11/25",
+    location: "Canteen",
+    photoUrl: "/Lost_sample.png",
+  },
+];
 const ItemList = () => {
   return (
     <div className="flex flex-wrap gap-15 p-8 items-center justify-between">
-      <ItemCard />
-      <ItemCard />
-      <ItemCard />
-      <ItemCard />
-      <ItemCard />
-      <ItemCard />
-      <ItemCard />
-      <ItemCard />
-      <ItemCard />
+      {ItemDataSample.map((item) => (
+        <ItemCard
+          key={item.id}
+          type={item.type}
+          itemName={item.name}
+          date={item.date}
+          location={item.location}
+          photoURL={item.photoUrl}
+        />
+      ))}
     </div>
   );
 };
