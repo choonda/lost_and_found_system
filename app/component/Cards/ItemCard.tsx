82dddfe0d3a9b229693147157c956b751122a3b8
newweijun <newweijun@graduate.utm.com>
2025-11-27 21:37:10 +0800

    responsive

--- app/component/Cards/ItemCard.tsx
+++ app/component/Cards/ItemCard.tsx
@@ -18,7 +18,7 @@ const ItemCard = ({
   photoURL,
 }: ItemCardProps) => {
   return (
-    <div className="w-[30%] h-fit bg-[#EBECF1] rounded-lg shadow-md">
+    <div className="w-full sm:w-[30%] h-fit bg-[#EBECF1] rounded-lg shadow-md">
       {/* UPPER */}
       <div className="relative">
         {/* PHOTO */}
@@ -26,8 +26,8 @@ const ItemCard = ({
           <Image
             src={photoURL}
             alt="lost item"
-            width={200}
-            height={200}
+            width={100}
+            height={100}
             className="rounded-lg w-full h-fit"
           />
           <div className="absolute bottom-2 left-2 bg-white px-4 py-1 rounded-full text-black font-semibold shadow z-40">
@@ -36,16 +36,24 @@ const ItemCard = ({
         </div>
       </div>
       {/* BOTTOM */}
-      <div className="flex flex-row w-full h-fit items-center px-3 py-2 justify-between">
-        <span className="text-[#808080] text-xl">{itemName}</span>
-        <div className="flex flex-row gap-2">
+      <div
+        className="flex w-full items-center px-3 py-2 
+             flex-col gap-2 
+             sm:flex-row sm:justify-between"
+      >
+        <span className="text-[#808080] text-xl w-full">{itemName}</span>
+
+        <div className="w-full justify-start flex flex-col gap-1 sm:flex-row sm:justify-end">
           <div className="flex items-center gap-1">
             <AiOutlineCalendar className="w-4 h-4 text-[#1A8A94]" />
-            <span className="text-[#808080] text-md">{date}</span>
+            <span className="text-[#808080] text-sm">{date}</span>
           </div>
+
           <div className="flex items-center gap-1">
-            <VscLocation className="w-5 h-5 text-red-500 " />
-            <span className="text-[#808080] text-md">{location}</span>
+            <VscLocation className="w-5 h-5 text-red-500" />
+            <span className="text-[#808080] text-sm truncate max-w-[100px] sm:max-w-[150px]">
+              {location}
+            </span>
           </div>
         </div>
       </div>
--- app/component/header.tsx
+++ app/component/header.tsx
@@ -6,37 +6,43 @@ import { VscAccount } from "react-icons/vsc";
 
 const Header = () => {
   return (
-    <div className="flex flex-row py-8 px-4 gap-20 border-b-[#C6D9DB] shadow-md shadow-gray-300 w-full h-fit items-center justify-between">
+    <div
+      className="
+  flex flex-wrap 
+  py-6 px-4 
+  gap-4 md:gap-16 
+  border-b border-[#C6D9DB] shadow-md shadow-gray-300 
+  w-full h-fit items-center justify-center
+
+"
+    >
       {/* ICON */}
-      <div className="flex flex-row h-fit items-center">
-        <div>
-          <Image src="/0nce.png" alt="logo" width={55} height={55} />
-        </div>
-        <div className="font-semibold text-5xl flex flex-row">
+      <div className=" items-center gap-2 sm:flex sm:flex-none">
+        <Image src="/0nce.png" alt="logo" width={50} height={50} />
+        <div className="font-semibold text-4xl flex flex-row">
           <h1 className="text-primarygreen">0</h1>
           <h1 className="text-primarygreen/50">nce</h1>
         </div>
       </div>
-      {/* SEARCH BAR */}
-      <div className=" flex flex-row h-fit w-fit">
-        <div className="flex items-center  bg-white rounded-full pl-4  shadow-md border-primarygreen border-2">
-          <div className="flex items-center gap-4">
-            <AiOutlineSearch className="w-10 h-10 text-primarygreen " />
+
+      <div className="flex flex-1 flex-row items-center gap-4 md:gap-16 justify-between w-full">
+        {/* SEARCH BAR */}
+        <div className="flex flex-1 min-w-0 md:w-[50%] w-full">
+          <div className="flex items-center w-full bg-white rounded-full pl-4 shadow-md border-primarygreen border-2">
+            <AiOutlineSearch className="w-8 h-8 text-primarygreen flex-none" />
             <input
-              className="border-primarygreen w-220 focus:outline-none placeholder-[A7A7A7] placeholder:text-lg"
+              className="flex-1 min-w-0 focus:outline-none placeholder-gray-400 text-base"
               type="text"
-              name="search"
               placeholder="Find something..."
-            ></input>
-            <button className="bg-buttongreen rounded-full py-3 px-6 hover:bg-[#006557] cursor-pointer">
-              <p className="font-semibold text-white text-lg">Search</p>
+            />
+            <button className="bg-buttongreen rounded-full py-2 px-5 flex-none hover:bg-[#006557] cursor-pointer">
+              <p className="font-semibold text-white">Search</p>
             </button>
           </div>
         </div>
-      </div>
-      {/* USER */}
-      <div className="flex flex-row gap-2 w-fit h-fit items-center">
-        <button className="cursor-pointer">
+
+        {/* USER */}
+        <button className="flex-none">
           <VscAccount className="w-10 h-10 text-primarygreen bg-white rounded-full" />
         </button>
       </div>
